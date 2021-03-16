c23c0dfda62c5fa7402d233d28ae1518b451cc73
guzelhamidova <guzel.hamidova07@mail.ru>
2021-03-16 18:54:39 +0600

    Initial commit

---
+++ src/com/company/Medic.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Medic extends Hero {
+    @Override
+    public void applySuperAbility() {
+        System.out.println(" “Madic применил суперспособность CRITICAL DAMAGE”.");
+
+    }
+}
